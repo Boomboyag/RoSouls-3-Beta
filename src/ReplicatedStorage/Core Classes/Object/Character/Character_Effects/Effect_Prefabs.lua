97903e81649666a130a68a8807733f64d703fee9
Boomboyag <gavyn.csvany11@gmail.com>
2024-05-04 11:34:01 -0400

    Changed the way strafing was enabled

--- src/ReplicatedStorage/Core Classes/Object/Character/Character_Effects/Effect_Prefabs.lua
+++ src/ReplicatedStorage/Core Classes/Object/Character/Character_Effects/Effect_Prefabs.lua
@@ -1,3 +1,9 @@
+-- Roblox services
+local replicatedStorage = game:GetService("ReplicatedStorage")
+
+-- Required scripts
+local movementType = require(replicatedStorage["Core Classes"].Enum.MovementType)
+
 -- The table of effects
 local effectTable = {
 
@@ -32,70 +38,15 @@ local effectTable = {
 				-- Called when changed to
 				StartFunction = function(character)
 				
-					-- The animations
-					local strafeLeft = character.coreAnimations.Strafing["Left"]
-					local strafeRight = character.coreAnimations.Strafing["Right"]
-				
-					-- Play the animations
-					strafeLeft:Play()
-					strafeRight:Play()
-				
-					-- Set the animation priority
-					strafeLeft.Priority = Enum.AnimationPriority.Movement
-					strafeRight.Priority = Enum.AnimationPriority.Movement
-				
-					-- Fade the animations to 0
-					strafeLeft:AdjustWeight(0.01, 0)
-					strafeRight:AdjustWeight(0.01, 0)
-				
-					-- Bind the strafe update to the render stepped
-					game:GetService("RunService"):BindToRenderStep("Strafe Update", Enum.RenderPriority.Character.Value + 1, function()
-
-						-- Get the current movement direction
-						local moveDir = character.humanoidRootPart.CFrame:VectorToObjectSpace(character:GetWorldMoveDirection())
-					
-						-- Get the individual axis
-						local x = moveDir.X
-						local z = moveDir.Z
-					
-						-- Make sure the character can strafe
-						if character.characterStats.canStrafe then
-						
-							-- Check what direction on the x-axis the character is moving
-							if x >= 0.5 then
-							
-								-- Fade the animations back to 0
-								strafeLeft:AdjustWeight(0.8 * x)
-								strafeRight:AdjustWeight(0.01)
-							
-							elseif x <= -0.5 then
-
-								-- Fade the animations back to 0
-								strafeLeft:AdjustWeight(0.01)
-								strafeRight:AdjustWeight(0.8 * math.abs(x))
-							else
-							
-								-- Fade the animations back to 0
-								strafeLeft:AdjustWeight(0.01, 0.1)
-								strafeRight:AdjustWeight(0.01, 0.1)
-							end
-						end
-					end)
+					-- Begin strafing
+					movementType.Strafing.StartFunction(character)
 				end,
 			
 				-- Called when changed from
 				EndFunction = function(character)
 				
-					-- The animations
-					local strafeLeft = character.coreAnimations.Strafing["Left"]
-					local strafeRight = character.coreAnimations.Strafing["Right"]
-				
-					-- Stop the animations
-					strafeLeft:Stop()
-					strafeRight:Stop()
-
-					-- Unbind the strafe
-					game:GetService("RunService"):UnbindFromRenderStep("Strafe Update")
+					-- Stop strafing
+					movementType.Strafing.EndFunction(character)
 				end
 			}
 		end,
