69de2503dd3980234e702ada1dc7749a6dd5a6b5
Boomboyag <gavyn.csvany11@gmail.com>
2024-07-22 16:47:20 -0400

    Created a new module that makes the character ragdoll on death

---
+++ src/ServerScriptService/Module_Initializer/Modules/Ragdoll_On_Death_Module/OnDeath.lua
@@ -0,0 +1,20 @@
+local module = {}
+
+module.Name = "Ragdoll on Death Module"
+
+-- The function to be called
+function module:CallFunction()
+
+    -- Make sure we can ragdoll
+    if self.Ragdoll ~= nil then
+        
+        -- Make the character ragdoll
+        self:Ragdoll()
+    else
+        warn("Ragdoll module not included!")
+    end
+
+    return 0
+end
+
+return module
---
+++ src/ServerScriptService/Module_Initializer/Modules/Ragdoll_On_Death_Module/init.lua
@@ -0,0 +1,26 @@
+-- Required services
+local replicatedStorage = game:GetService("ReplicatedStorage")
+
+-- Required folders
+local coreFolder = replicatedStorage:WaitForChild("Core Classes")
+local moduleFolder = coreFolder:WaitForChild("Object").Character.Modules
+local playerFolder = moduleFolder.Parent.Player.Required_Modules
+
+-- The action module
+local actionModule = script:WaitForChild("OnDeath")
+
+local module = {}
+
+-- The function to initialize the module
+function module:Init()
+    
+    -- Clone the module for other scripts to use
+    local module_ = actionModule:Clone()
+    module_.Parent = moduleFolder
+
+    -- Add the module to the character
+    local playerMod = actionModule:Clone()
+    playerMod.Parent = playerFolder
+end
+
+return module
