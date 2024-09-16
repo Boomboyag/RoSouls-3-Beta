963d98d76dd2daa9dd7b65909e1d99cd33584e0c
GC <gavyn.csvany11@gmail.com>
2024-09-16 17:35:02 -0400

    Created the weapon handler scripts without any real features

---
+++ src/ServerScriptService/Module_Initializer/Modules/Weapon_Handler_Module/Weapon_Handler.lua
@@ -0,0 +1,15 @@
+-- Required services
+local contentProvider = game:GetService("ContentProvider")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local module = {}
+
+-- The module name
+module.Name = "Weapon Handler"
+
+-- The init function
+function module:Init()
+
+end
+
+return module
---
+++ src/ServerScriptService/Module_Initializer/Modules/Weapon_Handler_Module/init.lua
@@ -0,0 +1,58 @@
+-- Required services
+local replicatedStorage = game:GetService("ReplicatedStorage")
+
+-- Required folders
+local coreFolder = replicatedStorage:WaitForChild("Core Classes")
+local moduleFolder = coreFolder:WaitForChild("Object").Character.Modules
+local playerFolder = moduleFolder.Parent.Player.Required_Modules
+
+-- The action module
+local actionModule = script:WaitForChild("Weapon_Handler")
+
+local module = {}
+
+-- The function to create the events that load weapons
+function LoadWeaponEvents()
+    
+    -- The events
+    local remoteFunction = Instance.new("RemoteFunction")
+    remoteFunction.Name = "Load_Weapons_Remote"
+    local bindableFunction = Instance.new("BindableFunction")
+    bindableFunction.Name = "Load_Weapons_Bindable"
+    -- Get the remote folder
+    local remoteFolder = replicatedStorage:FindFirstChild("Remote")
+    if not remoteFolder then
+        
+        remoteFolder = Instance.new("Folder")
+        remoteFolder.Name = "Remote"
+        remoteFolder.Parent = replicatedStorage
+    end
+    remoteFunction.Parent = remoteFolder
+
+    -- Get the bindable folder
+    local bindableFolder = replicatedStorage:FindFirstChild("Bindable")
+    if not bindableFolder then
+        
+        bindableFolder = Instance.new("Folder")
+        bindableFolder.Name = "Bindable"
+        bindableFolder.Parent = replicatedStorage
+    end
+    bindableFunction.Parent = bindableFolder
+end
+
+-- The function to initialize the module
+function module:Init()
+    
+    -- Clone the module for other scripts to use
+    local module_ = actionModule:Clone()
+    module_.Parent = moduleFolder
+
+    -- Add the module to the character
+    local playerMod = actionModule:Clone()
+    playerMod.Parent = playerFolder
+
+     -- || LOADING WEAPONS ||
+     LoadWeaponEvents()
+end
+
+return module
